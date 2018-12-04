00894007e69f3269456586a024f1fd8b6eda5838
Dmitry Kalashnikov <dmitry@calibermind.com>
2018-12-04 20:27:49 +0300

    version info upd

--- simple_salesforce/__version__.py
+++ simple_salesforce/__version__.py
@@ -2,13 +2,13 @@
 
 This file shamelessly taken from the requests library"""
 
-__title__ = 'simple-salesforce'
-__description__ = 'A basic Salesforce.com REST API client.'
-__url__ = 'https://github.com/simple-salesforce/simple-salesforce'
+__title__ = 'simple-salesforce-tronok'
+__description__ = 'A basic Salesforce.com REST API client with tweaks.'
+__url__ = 'https://github.com/tronok/simple-salesforce'
 __version__ = '0.74.2'
-__author__ = 'Nick Catalano'
-__author_email__ = 'nickcatal@gmail.com'
+__author__ = 'Dmitry Kalashnikov'
+__author_email__ = 'kalashnikovdm@gmail.com'
 __license__ = 'Apache 2.0'
-__maintainer__ = 'Demian Brecht'
-__maintainer_email__ = 'demianbrecht@gmail.com'
+__maintainer__ = 'Dmitry Kalashnikov'
+__maintainer_email__ = 'kalashnikovdm@gmail.com'
 __keywords__ = 'python salesforce salesforce.com'
